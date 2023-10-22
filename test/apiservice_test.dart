642248dd59d303920037d8c3f1526dcf1e639bc7
Uday-kiran9147 <udaykiran9147@gmail.com>
2023-10-22 18:19:29 +0530

    Unit tests

--- test/apiservice_test.dart
+++ test/apiservice_test.dart
@@ -1,7 +1,20 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:math';
 
-void main(){
-  test("fetch data from API", () {
-    
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shopping_ui/services/apiservices.dart';
+
+void main() {
+  group("API Testing on products and categories ", () {
+    test("Get products from products list ", () async {
+      final productslist = await ApiService().getProducts();
+
+      expect(productslist!.length, 20); // we expect 20 products
+    });
+
+    test("Get categories from categories list ", () async {
+      final categorieslist = await ApiService().getCategories();
+
+      expect(categorieslist.length, 4); // we expect 5 categories
+    });
   });
 }
